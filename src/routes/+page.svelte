be2be2e8d887ca10b74285d9eaa929aaa87cc121
Michael Sebastian Søby <michaels@goodiebox.dk>
2023-01-24 22:38:33 +0100

    When picking a number that has already been picked remove from list, also show start instead of undefined before a number has been picked

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,8 +11,10 @@
 	function clickedNumber(num) {
 		// console.log('los eventos', event.target.innerHTML)
 		console.log('received number', num);
-		pickedNumbers.push(num);
-		pickedNumbers = pickedNumbers;
+		pickedNumbers = pickedNumbers.includes(num)
+			? pickedNumbers.filter((n) => n !== num)
+			: [...pickedNumbers, num];
+
 		console.log('hisory', pickedNumbers);
 	}
 </script>
@@ -31,7 +33,7 @@
 		{/each}
 	</div>
 	<div class="flex flex-col items-center justify-center h-screen w-1/2">
-		<p class="text-9xl">{pickedNumbers[pickedNumbers.length - 1]}</p>
+		<p class="text-9xl">{pickedNumbers.length ? pickedNumbers[pickedNumbers.length - 1]: 'Start'}</p>
 		<p class="text-base text-blue-600 pt-10">{pickedNumbers}</p>
 	</div>
 </div>
