ade358bdf1619be1af895d7ddf4fc893c9cc62eb
Michael Sebastian Søby <michaels@goodiebox.dk>
2023-01-24 22:23:52 +0100

    Change latest number to 9xl and add element with history below

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,40 +3,41 @@
 </h1>
  -->
 <script>
-  // create array with 1-90
-  let numbers = Array.from(Array(91).keys()).slice(1)
+	// create array with 1-90
+	let numbers = Array.from(Array(91).keys()).slice(1);
 
-  let pickedNumbers = []
+	let pickedNumbers = [];
 
-  function clickedNumber(num) {
-    // console.log('los eventos', event.target.innerHTML)
-    console.log('received number', num)
-    pickedNumbers.push(num)
-    pickedNumbers = pickedNumbers
-    console.log('hisory', pickedNumbers)
-  }   
+	function clickedNumber(num) {
+		// console.log('los eventos', event.target.innerHTML)
+		console.log('received number', num);
+		pickedNumbers.push(num);
+		pickedNumbers = pickedNumbers;
+		console.log('hisory', pickedNumbers);
+	}
 </script>
 
 <div class="flex">
-  <div class="flex flex-row flex-wrap space-between w-1/2" >
-    {#each numbers as number}
-      <!-- {#if pickedNumbers.includes(number)}
+	<div class="flex flex-row flex-wrap space-between w-1/2">
+		{#each numbers as number}
+			<!-- {#if pickedNumbers.includes(number)}
       {/if} -->
-      <p on:click={() => clickedNumber(number)} class="px-4 {pickedNumbers.includes(number) ? 'bg-red-200' : ''}">{number}</p>
-      
-    {/each}
-  </div>
-  <div class="flex items-center justify-center h-screen w-1/2">
-    <p class="text-5xl">{pickedNumbers[pickedNumbers.length-1]}</p>
-  </div>
+			<p
+				on:click={() => clickedNumber(number)}
+				class="px-4 {pickedNumbers.includes(number) ? 'bg-red-200' : ''}"
+			>
+				{number}
+			</p>
+		{/each}
+	</div>
+	<div class="flex flex-col items-center justify-center h-screen w-1/2">
+		<p class="text-9xl">{pickedNumbers[pickedNumbers.length - 1]}</p>
+		<p class="text-base text-blue-600 pt-10">{pickedNumbers}</p>
+	</div>
 </div>
 
-
-
-
-
 <style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.200);
-  }
+	:global(html) {
+		background-color: theme(colors.gray.200);
+	}
 </style>
